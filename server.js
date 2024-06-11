4762826a964c17d83df48fd32a02b09cce7489d0
Autobots6991 <94107407+Autobots6991@users.noreply.github.com>
2024-06-11 18:40:20 +0700

    Update

--- server.js
+++ server.js
@@ -6,16 +6,8 @@ const HOST = "localhost";
 const PORT = 88;
 const session = require("cookie-session")
 const uuid = require("uuid")
-const cors = require("cors")
+const cors = require("cors");
 
 
-app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-    })
-)
-app.use(express.static("wwwroot"))
-app.use(session({secret:"abc", maxAge: 24*60*60*1000}))
-app.use(require("./script.js"))
+app.use(express.static(__dirname))
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}!`))
